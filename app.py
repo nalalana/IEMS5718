45576299e72fb69abe4c997ebf7896d5d4872d80
nalalana <3392284556@qq.com>
2025-02-16 16:45:03 +0800

    main

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
