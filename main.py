9787bf482e9ee9582f8800aa7ff62dfdea734401
nalalana <3392284556@qq.com>
2025-02-16 17:19:01 +0800

    main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import os
+
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
-@app.get('/')
+
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return {'message': 'Hello World'}
+    # 返回 frontend/index.html 文件
+    file_path = os.path.join(os.getcwd(), 'frontend', 'index.html')
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
 
 if __name__ == "__main__":
     import uvicorn
