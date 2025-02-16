cb924855514925781c1bd795c8aad90f718e05a8
nalalana <3392284556@qq.com>
2025-02-16 12:33:16 +0800

    uvicorn

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get('/')
 def root():
     return {'message': 'Hello World'}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
